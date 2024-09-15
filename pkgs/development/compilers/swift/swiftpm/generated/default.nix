f7a822851b14111db866a8c0a356714c559dbc58
Joe Diragi <joseph.diragi@icloud.com>
2024-09-15 11:44:23 -0400

    WIP: Build succeeds

--- pkgs/development/compilers/swift/swiftpm/generated/default.nix
+++ pkgs/development/compilers/swift/swiftpm/generated/default.nix
@@ -8,7 +8,7 @@
     "swift-collections" = "0i19czvb1j0xjbg238k802g72dpsa9mvxn9wf16jc3bgckvgbn2q";
     "swift-crypto" = "1i9sshrkckq8kx8j9nap93xrnzmv1q9zwjjlmg6735x7vigf645w";
     "swift-driver" = "1m02089427i3ln2663r270vzkcbydy24gy5pc2wd9j1sh7yp153d";
-    "swift-llbuild" = "03czswc2vwz2711qid7jc0l9119fcrqgq585j084v9a9x9i2j4n3";
+    "llbuild" = "03czswc2vwz2711qid7jc0l9119fcrqgq585j084v9a9x9i2j4n3";
     "swift-system" = "0402hkx2q2dv27gccnn8ma79ngvwiwzkhcv4zlcdldmy6cgi0px7";
     "swift-tools-support-core" = "0xh84x25ar5vfvjy58yzamphxm19g713nda5jqx9kjfssdpfv9rk";
     "Yams" = "1jf3k2lm5b9i8m9bpnzhqa8mnc3qzj91icv5ykjplk3vy0idz3q1";
